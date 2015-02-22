d5858bd215f50e51cc016de267bb204dcb6d379a
Glossawy <4606234+Glossawy@users.noreply.github.com>
2015-02-21 23:39:30 -0500

    Solution to Client/Server Cache Desync (Cache No longer Exists technically... we just use File IO as little as necessary)
    Server can now be paused as an added security measure. This allows the Admins to edit the YAML files without risk of the Server overriding them. As a side effect, Update files will now need to be deleted before a new one can be created, Scorekeeper will block until it is deleted by WriteServer, this is another synchronization measure. It becomes difficult to handle when we are trying to mimic Network IO (which has protocols) using File IO.
    
    Also some more documentation, cleanup and minor Conversion to Ruby idiomatic programming.
    
    I do have a dissent in WriteServer and I would like to note how this system is not scalable nor able to adapt to changes in setup should that be necessary.

--- WriteServer.rb
+++ WriteServer.rb
@@ -3,12 +3,30 @@ require './Tournament'
 
 Shoes.app(title: "WriteServer - TinyTron", width: 800, height: 600, resizable: false) do
 
+
 	@updateDirectory = "NONE"
 	@tourneyPath = "NONE"
+	@paused = false
 
+	# Top -- System Info/Server State
 	stack(height: 0.5, width: 1.0, margin: 10) do
-		subtitle "System Info"
+		# Title + Server State and Button
+		flow do
+			@infoTitle = subtitle "System Info - Server Status: Running"
+			@pauseButton = button("Toggle On/Off")
+			@pauseButton.displace(40, 10)
+			@pauseButton.click() do
+				if(!@paused)
+					@infoTitle.text = "System Info - Server Status: Paused"
+					@paused = true
+				else
+					@infoTitle.text = "System Info - Server Status: Running"
+					@paused = false
+				end
+			end
+		end
 
+		# System Environment Description. Basically Dumps Every System Environmental Variable
 		flow(scroll: true, height: 0.8, width: 1.0, margin_bottom: 10) do
 			border black, strokewidth: 2.5
 			stack(scroll: true, height: 1.0, width: 1.0, margin: 10) do
@@ -22,6 +40,7 @@ Shoes.app(title: "WriteServer - TinyTron", width: 800, height: 600, resizable: f
 		end
 	end
 
+	# Bottom -- Server Starting Parameters and Cache Contents
 	stack(height: 0.5, width: 1.0, margin: 10) do
 		subtitle "TinyTron Server Parameters"
 		flow(height: 0.8, width: 1.0) do
@@ -34,43 +53,61 @@ Shoes.app(title: "WriteServer - TinyTron", width: 800, height: 600, resizable: f
 		end
 	end
 
+	# Should we move most of this stuff out? The only thing being done every iteration
+	# is Update Processing and Param Updates
+	#
+	# Initialization should be done ON INITIALIZATION. Not AFTER INITIALIZATION. Not even a single second after.
 	every(1) do
-
+		# Initialize Path to Tournament File
 		if(@tourneyPath == "NONE")
 			alert("Please select the tournament file.")
 			@tourneyPath = ask_open_file()
 
 			plog @tourneyPath
-
-			File.open(@tourneyPath, "r+") do |f|
-				@tourneyCache = YAML.load(f)
-			end
+			update_cache
 		end
 
+		# Initialize Path to Update Directory
 		if(@updateDirectory == "NONE")
 			alert("Please select the tournament update directory.")
 			@updateDirectory = ask_open_folder()
 		end
 
-		if(processUpdate("A") || processUpdate("B"))
-			File.open(@tourneyPath,"w") do |h|
-				h.write @tourneyCache.to_yaml()
+		# If not paused, check for updates and then update the GUI
+		if(!@paused)
+			if((val = update_check) != nil)
+				process val
 			end
-		end
 
-		update_param_info
+			update_param_info
+		end
 	end
 
-	def processUpdate(id)
+	#######################################################################################
+	# Process Update File by ID ("A" or "B")
+	#
+	# Locks Update File (to prevent Client/Server Desync) and proceeds to add player scores
+	# based on what numerical ranking the player was given by Scorekeeper. The index is then
+	# incremented and the resuilts written out to the Tournament File.
+	#
+	# The Tournament File is locked for the duration of this process.
+	# Raises: IOError if Update File does not exist.
+	######################################################################################
+	def process(id)
+		if(!File.file?("#{@updateDirectory}\\Update.#{id}"))
+			raise IOError, "Update for #{id} does not exist!"
+		end
 
-		if(File.file?("#{@updateDirectory}\\Update.#{id}"))
-			yamlData = File.open("#{@updateDirectory}\\Update.#{id}", "r+")
-			updateData = YAML.load(yamlData)
-			plog "YAML Update Data:\n#{updateData}"
+		yaml_file = File.open("#{@updateDirectory}\\Update.#{id}", "r+")
+		yaml_data = YAML.load yaml_file
+		plog "YAML Update Data:\n#{yaml_data}"
 
-			(0...updateData.size).each do |i|
+		File.open(@tourneyPath, "r+") do |f|
+			@tourneyCache = YAML.load f
 
-				case updateData[i][1]
+			(0...yaml_data.size).each do |i|
+				score = 0
+				case yaml_data[i][1]
 					when 1
 						score = 400
 					when 2
@@ -79,23 +116,46 @@ Shoes.app(title: "WriteServer - TinyTron", width: 800, height: 600, resizable: f
 						score = 200
 					when 4
 						score = 100
-					else score = 0
 				end
 
-				@tourneyCache.addScore(updateData[i][0],score)
+				@tourneyCache.addScore(yaml_data[i][0], score)
 			end
 
 			@tourneyCache.indexA = @tourneyCache.indexA + 1 if(id == "A")
 			@tourneyCache.indexB = @tourneyCache.indexB + 1 if(id == "B")
 
-			yamlData.close()
-			File.delete("#{@updateDirectory}\\Update.#{id}")
-			return true
+			f.seek 0
+			f.write @tourneyCache.to_yaml
 		end
 
-		return false
+		yaml_file.close
+		File.delete(yaml_file)
 	end
 
+	#######################################################################################
+	# Updates Tournament Cache from File
+	######################################################################################
+	def update_cache(file = @tourneyPath)
+		File.open(file, "r+") do |h|
+			@tourneyCache = YAML.load h
+		end
+	end
+
+	#######################################################################################
+	# Returns the Index of the next Update to process
+	# Returns nil otherwise
+	######################################################################################
+	def update_check
+		if(File.file?("#{@updateDirectory}\\Update.A"))
+			return "A"
+		elsif (File.file?("#{@updateDirectory}\\Update.B"))
+			return "B"
+		end
+	end
+
+	#######################################################################################
+	# Updates Inscriptions
+	######################################################################################
 	def update_param_info
 		info = {"Update Dir" => @updateDirectory, "Tourney Path" => @tourneyPath, "Current Cache" => "\n#{@tourneyCache}"}
 
